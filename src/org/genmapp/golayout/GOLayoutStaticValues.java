576161624267e8b0b149a2f874d46e3e0c5df3b6
AlexanderPico <apico@gladstone.ucsf.edu>
2011-06-25 18:06:45 +0000

    added database dir locations

--- src/org/genmapp/golayout/GOLayoutStaticValues.java
+++ src/org/genmapp/golayout/GOLayoutStaticValues.java
@@ -18,4 +18,6 @@ public class GOLayoutStaticValues {
     protected static final String MF_CODE = "Tm";
     protected static final String[] speciesList = {"Human", "Mouse", "Yeast", "Fruitfly", "Celegans","Zebrafish"};
     protected static final String bridgedbSpecieslist = "http://svn.bigcat.unimaas.nl/bridgedb/trunk/org.bridgedb.bio/resources/org/bridgedb/bio/organisms.txt";
+    protected static final String genmappcsDatabaseDir = "http://genmapp.org/genmappcs/databases/";
+    protected static final String bridgedbDerbyDir = "http://bridgedb.org/data/gene_database/";
 }
