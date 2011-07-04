8b25330775abf8d6719dd5c7aac6d99345219b19
ChaoZhang <ChaoZhang@23aac120-1128-46ae-97c9-9e165d6aae65>
2011-07-04 22:31:29 +0000

    Finish db downloading

---
+++ src/org/genmapp/golayout/utils/FileDownloadDialog.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.genmapp.golayout.utils;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Chao
+ */
+
+public class FileDownloadDialog extends JDialog implements ActionListener {
+    public  List<String> downloadFileList;
+    private JPanel optionPane;
+    public  JLabel messageLabel;
+    public  JButton comfirmButton;
+
+    public FileDownloadDialog(JFrame aFrame, List<String> downloadDBList) {
+        super(aFrame, true);
+        downloadFileList = downloadDBList;
+        setTitle("Downloading databases");
+
+        optionPane = new JPanel();
+        messageLabel = new JLabel();
+        optionPane.add(messageLabel);
+        comfirmButton = new JButton("Please wait ......");
+        comfirmButton.setEnabled(false);
+        comfirmButton.addActionListener(this);
+        optionPane.add(comfirmButton);
+        this.add(optionPane);
+        //Handle window closing correctly.
+        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        new DownloadThread(this).start();
+    }
+
+    /** This method handles events for the text field. */
+    public void actionPerformed(ActionEvent e) {
+        this.dispose();
+    }
+}
+
+/**
+ *
+ * @author Anurag Sharma
+ */
+class DownloadThread extends Thread {
+
+    private FileDownloadDialog sp;
+
+    public DownloadThread(FileDownloadDialog sp) {
+        this.sp = sp;
+    }
+
+    public void run() {
+        try {
+            System.out.println("starting download");
+            for(String fileName:sp.downloadFileList) {
+                Downloader d = new Downloader();
+                d.download(fileName);
+                int progress = d.getProgress();
+                while (progress < 99) {
+                    System.out.println(fileName + ": " + progress + "%");
+                    sp.messageLabel.setText(fileName + ": " + progress + "%");
+                    Thread.sleep(500);
+                    progress = d.getProgress();
+
+                }
+                sp.messageLabel.setText(fileName + ": 100%");
+                
+                // must wait for completion of uncompression before giving up thread
+                d.waitFor();
+            }
+            sp.comfirmButton.setText("Finished!");
+            sp.comfirmButton.setEnabled(true);
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
